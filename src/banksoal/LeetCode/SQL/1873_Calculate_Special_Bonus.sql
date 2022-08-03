15f75766fd491b9a9ba863490a22cc11a5e6dc01
sogumontar.simangunsong <sogumontar.simangunsong@dana.id>
2022-08-03 21:25:34 +0700

    case in sql

---
+++ src/banksoal/LeetCode/SQL/1873_Calculate_Special_Bonus.sql
@@ -0,0 +1,8 @@
+-- Write your MySQL query statement below
+SELECT employee_id,
+CASE
+    WHEN employee_id%2 = 0 THEN 0
+    WHEN name  LIKE 'M%' THEN  0
+    ELSE salary end
+as bonus
+FROM Employees
